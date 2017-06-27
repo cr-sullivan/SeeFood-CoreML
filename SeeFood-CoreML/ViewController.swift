ef68d76f57f7351d18130e45bbcc1f2ebdc85a3f
Angela Yu <angelayu@Angelas-MacBook-Pro.local>
2017-06-27 13:04:57 +0100

    Initial Commit

---
+++ SeeFood-CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeFood-CoreML
+//
+//  Created by Angela Yu on 27/06/2017.
+//  Copyright © 2017 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
