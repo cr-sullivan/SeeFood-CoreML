ce6d95e0d35896e9e06a99f1a637c38d8bb97304
ChrisSullivan <cr_sullivan@hotmail.com>
2018-01-04 19:47:08 +1300

    Show most likely result

--- SeeFood-CoreML/ViewController.swift
+++ SeeFood-CoreML/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Social
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var classificationResults : [VNClassificationObservation] = []
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func detect(image: CIImage) {
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("can't load ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+                }
+            
+//                if topResult.identifier.contains("hotdog") {
+//                    DispatchQueue.main.async {
+//                        self.navigationItem.title = "Hotdog!"
+//                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+//                        self.navigationController?.navigationBar.isTranslucent = false
+//                    }
+//            else {
+//                DispatchQueue.main.async {
+//                    self.navigationItem.title = "Not Hotdog!"
+//                    self.navigationController?.navigationBar.barTintColor = UIColor.red
+//                    self.navigationController?.navigationBar.isTranslucent = false
+//                }
+//            }
+                DispatchQueue.main.async {
+                        self.navigationItem.title = topResult.identifier
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do { try handler.perform([request]) }
+        catch { print(error) }
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("couldn't convert uiimage to CIImage")
+            }
+            
+            detect(image: ciImage)
+        }
+    }        
 
-
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+   
 }
 
